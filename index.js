d0145dab85bc806a0e52df13c0aeb1661a67868a
Hyunrae Kim <hyunrae.kim@gmail.com>
2018-03-21 18:50:55 -0500

    Finished initial implementation

--- index.js
+++ index.js
@@ -3,75 +3,111 @@
 process.env.DEBUG = 'actions-on-google:*';
 const { DialogflowApp } = require('actions-on-google');
 const functions = require('firebase-functions');
-const http = require('http');
-const tokenURL = "https://login-sandbox.safetrek.io/oauth/token";
 const baseURL = "https://api-sandbox.safetrek.io/v1";
+const axios = require('axios');
 
 const WELCOME_ACTION = 'input.welcome';
 const CALL_ACTION = 'input.call';
 const HELP_TYPE_ARGUMENT = 'help_type';
 const REQUEST_ACTION = 'input.request';
-const CLIENT_ID = "gk1nFtbQr4pBpJD0rzAp3vaSi555sm4s";
-const CLIENT_SECRET = "eWTSj_izMvD3nBJFXxkRDZF4aXDGKofYRZyzw_31oer31kuoY6";
-const REDIRECT_URI = "https://oauth-redirect.googleusercontent.com/r/safetrek-68cd7";
-
 
 exports.SafeTrek = functions.https.onRequest((request, response) => {
-  const app = new DialogflowApp({request, response});
-  console.log('Request headers: ' + JSON.stringify(request.headers));
-  console.log('Request body: ' + JSON.stringify(request.body));
 
-  // Make a silly name
+  const app = new DialogflowApp({request, response});
+
   function callSafetrek (app) {
+
     if (app.isPermissionGranted()) {
-            const address = app.getDeviceLocation().coordinates;
-            console.log("latitude is: " + JSON.stringify(address.latitude) + " and longitude is: " + address.longitude);
+            const address = app.getDeviceLocation();
             if (address) {
-              const helpType = JSON.stringify(app.getArgument('help_type'));
-              let accessToken = app.getUser().accessToken;
+              const helpType = app.getArgument('help_type');
+              let accessToken = app.getUser().access_token;
               //app.tell('You requested for ' + helpType.replace(/\"/g, "") + " help");
-              //app.tell(JSON.stringify(accessToken));
-              requestToken(app, accessToken);
+              //requestToken(app, accessToken, address);
+              app.tell("Help will be on your way");
+              makeCall(accessToken, address, helpType).then((output) => {
+                console.log("SafeTrek Response: " + JSON.stringify(output));
+                app.tell("Help will be sent");
+              }).catch((error) => {
+                console.log("SafeTrek Error: " + JSON.stringify(error));
+                app.tell("Error calling for help");
+              });
+
             }
             else {
-                // Note: Currently, precise locaton only returns lat/lng coordinates on phones and lat/lng coordinates
-                // and a geocoded address on voice-activated speakers.
-                // Coarse location only works on voice-activated speakers.
-              app.tell('Sorry, I could not figure out where you are. Check your permissions');
+              app.tell("Sorry, it doesn't seem like I can find your address.");
             }
         } else {
-            app.tell('Sorry, I need permissions to send help to your location. Try again.');
+            app.tell('Sorry, I need your permission to locate you and send help.');
         }
   }
 
 
-  function requestToken (app, accessToken) {
+  function makeCall (accessToken, address, helpType) {
+    var coordinates = address.coordinates;
+    helpType = String(helpType);
+    var fire = false;
+    var police = false;
+    var medical = false;
 
-({"grant_type": "authorization_code",
-      "code": authorizationCode,
-      "client_id": CLIENT_ID,
-      "client_secret": CLIENT_SECRET,
-      "redirect_uri": REDIRECT_URI });
+     if (helpType === "fire") {
+       fire = true;
+     } else if (helpType === "law_enforcement") {
+       police = true;
+     } else if (helpType === "medical") {
+       medical = true;
+     } else if (helpType === "all") {
+       fire = true;
+       police = true;
+       medical = true;
+     }
+
+      const header = {
+        	"headers": {
+        		"Authorization": "Bearer " + accessToken,
+        		"Content-Type": "application/json",
+        	}
+        };
+
+      const obj = {
+          "services": {
+            "police": fire,
+            "fire": police,
+            "medical": medical
+          },
+          "location.coordinates": {
+            "lat": coordinates.latitude,
+            "lng":coordinates.longitude,
+            "accuracy": 5
+          }
+
+         //  "location.address": {
+         //   "line1": address.postalAddress.addressLines[0],
+         //   "city": address.city,
+         //   "state": address.postalAddress.administrativeArea,
+         //   "zip": address.zipCode
+         // }
+        };
+
+        return axios.post('https://api-sandbox.safetrek.io/v1/alarms', obj, header)
+
+        .then((response) => {
+          return Promise.resolve(response)
+        })
+        .catch(error => {
+          return Promise.reject(error)})
+
 
   }
-
-  function makeCall (app) {
-  }
-
 
   function requestPermission (app) {
     app.askForPermission('To locate you', app.SupportedPermissions.DEVICE_PRECISE_LOCATION);
   }
 
-  function welcome (app) {
-    app.ask("Welcome to Safetrek. Are you in need of any help?");
-
-  }
 
   let actionMap = new Map();
     actionMap.set(REQUEST_ACTION, requestPermission);
-    actionMap.set(WELCOME_ACTION, welcome);
     actionMap.set(CALL_ACTION, callSafetrek);
 
-  app.handleRequest(actionMap);
+    app.handleRequest(actionMap);
 });
